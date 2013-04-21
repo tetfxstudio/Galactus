e192dd3fb87f7de16ac333e42522433ab3c8e2f4
Brian Seward <brian@Brians-Super-Tiny-Laptop.local>
2013-04-20 17:21:55 -0700

    Remove text shadow from stars, since it crippled rendering

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,7 @@
 $(function(){
 
 	var starterStar = $(".star").eq(0);
-	for( var i = 20; i > 0; i-- ) {
+	for( var i = 100; i > 0; i-- ) {
 		starterStar.after(starterStar.clone().css({
 			left: Math.random()*100+"%",
 			top: Math.random()*100+"%",
