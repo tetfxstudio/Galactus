477d5d0e71c94a3a65e09bbea13bf5fbaa12fe39
Brian Seward <brian@Brians-Super-Tiny-Laptop.local>
2013-04-20 17:18:32 -0700

    Add stars via CSS, because it's funny

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,11 @@
 $(function(){
-	  $.backstretch("img/visit-space.jpg");
+
+	var starterStar = $(".star").eq(0);
+	for( var i = 20; i > 0; i-- ) {
+		starterStar.after(starterStar.clone().css({
+			left: Math.random()*100+"%",
+			top: Math.random()*100+"%",
+			fontSize: Math.floor(Math.random()*30+20)
+		}));
+	}
 });
